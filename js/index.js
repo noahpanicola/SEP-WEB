5b4ff69a2e54d6921c5b5070c4793aff7e1dd858
Noah Panicola <nmp5h8@mail.missouri.edu>
2017-11-06 14:04:48 -0600

    stores user in database

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 var indexApp = angular.module("IndexApp", []);
 indexApp.controller('indexController', function ($scope, $http) {
-    var uid = 1;
+
+    var url_string = window.location.href
+    var url = new URL(url_string);
+    var uid = url.searchParams.get("uid");
+
     $http.get('http://localhost:8080/property/uid=' + uid).
         success(function(data, status, headers, config){
             $scope.properties = data;
             $scope.tenants = data[0].tenants;
-            console.log(data.tenants);
+
             console.log(data);
         }).error(function(data, status, headers, config){
-            console.log("There was an error");
+            console.log("There was an error getting the user's properties.");
         });
+
+    $http.get('http://localhost:8080/user/' + uid).
+        success(function(data, status, headers, config){
+        $scope.user = data;
+        console.log(data);
+    }).error(function(data, status, headers, config){
+        console.log("There was an error getting the user.");
+    });
 });
